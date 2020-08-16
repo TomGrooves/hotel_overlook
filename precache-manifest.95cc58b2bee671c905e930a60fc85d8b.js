c231243daa0e15fe5d85a925a20d794a4e580118
TomGrooves <kasperfryd@gmail.com>
2020-08-16 13:16:22 +0200

    Updates

---
+++ precache-manifest.95cc58b2bee671c905e930a60fc85d8b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcaf0df10158962e90fbb69f2ba65efe",
+    "url": "/hotel_overlook/index.html"
+  },
+  {
+    "revision": "4a3e9413a485cfb2be15",
+    "url": "/hotel_overlook/static/css/main.bbba6ef9.chunk.css"
+  },
+  {
+    "revision": "13b33c2b755ea1f2017b",
+    "url": "/hotel_overlook/static/js/2.f82cf29f.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/hotel_overlook/static/js/2.f82cf29f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a3e9413a485cfb2be15",
+    "url": "/hotel_overlook/static/js/main.64b78452.chunk.js"
+  },
+  {
+    "revision": "a5b9ca947394a2f2cea1",
+    "url": "/hotel_overlook/static/js/runtime-main.940296f6.js"
+  },
+  {
+    "revision": "3a698c4dbca91ff3b318aced07e4b90f",
+    "url": "/hotel_overlook/static/media/center-square-wroclaw.3a698c4d.jpg"
+  },
+  {
+    "revision": "71af9708043b4b0fcc23c588b44defbf",
+    "url": "/hotel_overlook/static/media/city-houses-reykjavik.71af9708.jpg"
+  },
+  {
+    "revision": "090e74bc0fdbed51af05bb3a4b18683d",
+    "url": "/hotel_overlook/static/media/fishmarket-hamborg.090e74bc.jpg"
+  },
+  {
+    "revision": "85e475a5657e03c62c2a032ec45a6b61",
+    "url": "/hotel_overlook/static/media/frankfurt-skyline-germany.85e475a5.jpg"
+  },
+  {
+    "revision": "b2e1641780db4016c23ee06e365ed2e9",
+    "url": "/hotel_overlook/static/media/headernew.b2e16417.svg"
+  },
+  {
+    "revision": "d9b45634b4bfd4e8bf2a63ea9ba98e92",
+    "url": "/hotel_overlook/static/media/hotel-overlook-logo-white.d9b45634.svg"
+  },
+  {
+    "revision": "d4c977e32b316a2f76ec5124e4fac90e",
+    "url": "/hotel_overlook/static/media/star-icon.d4c977e3.svg"
+  }
+]);
